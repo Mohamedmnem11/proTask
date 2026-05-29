d5655706aa3ed0e6f65fdce2d8adebec89a66897
mhmdbdalmnmmhmwd71@gmail.com <mhmdbdalmnmmhmwd71@gmail.com>
2026-05-29 05:56:10 +0300

    go

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mmnem0553_db_user:pir5Zg7NGabezWNL@ac-q2cogty-shard-00-00.iz7raxb.mongodb.net:27017,ac-q2cogty-shard-00-01.iz7raxb.mongodb.net:27017,ac-q2cogty-shard-00-02.iz7raxb.mongodb.net:27017/booking?ssl=true&replicaSet=atlas-9phvwa-shard-0&authSource=admin';
+// تأكد من وجود رابط قاعدة البيانات (استخدم القيمة الافتراضية إذا لم توجد في البيئة)
+const MONGODB_URI: string = (process.env.MONGODB_URI as string) || 
+  'mongodb://mmnem0553_db_user:pir5Zg7NGabezWNL@ac-q2cogty-shard-00-00.iz7raxb.mongodb.net:27017,ac-q2cogty-shard-00-01.iz7raxb.mongodb.net:27017,ac-q2cogty-shard-00-02.iz7raxb.mongodb.net:27017/booking?ssl=true&replicaSet=atlas-9phvwa-shard-0&authSource=admin';
 
 if (!MONGODB_URI) {
   throw new Error('❌ MONGODB_URI is not defined in environment variables');
