ef054f503573a7fbe441881551157eeb54c01835
mhmdbdalmnmmhmwd71@gmail.com <mhmdbdalmnmmhmwd71@gmail.com>
2026-05-29 05:50:22 +0300

    go

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mmnem0553_db_user:pir5Zg7NGabezWNL@ac-q2cogty-shard-00-00.iz7raxb.mongodb.net:27017,ac-q2cogty-shard-00-01.iz7raxb.mongodb.net:27017,ac-q2cogty-shard-00-02.iz7raxb.mongodb.net:27017/booking?ssl=true&replicaSet=atlas-9phvwa-shard-0&authSource=admin';
 
 if (!MONGODB_URI) {
-  throw new Error('❌ MONGODB_URI غير موجود في ملف .env');
+  throw new Error('❌ MONGODB_URI is not defined in environment variables');
 }
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
